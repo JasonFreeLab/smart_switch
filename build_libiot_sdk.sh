f8026a3ec4a01c2107ccf76569ec45eb1bdb4acb
JasonFreeLab <1578989199@qq.com>
2023-11-24 00:13:23 +0800

    update

--- build_libiot_sdk.sh
+++ build_libiot_sdk.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 echo "******************** Build ali-smartliving-device-sdk-c lib START ********************"
-cd ../$1/ali-smartliving-device-sdk-c
-make distclean
 
 echo IDF_TARGET = $2
 
-if [ $2 == esp32 ]; then
-    number=2
-elif [ $2 == esp32c3 ]; then
-    number=3
-elif [ $2 == esp32c6 ]; then
-    number=4
-elif [ $2 == esp32s2 ]; then
-    number=5
-elif [ $2 == esp32s3 ]; then
-    number=6
-elif [ $2 == esp8266 ]; then
-    number=7
+cd ../$1/ali-smartliving-device-sdk-c
+
+if [ -f ".config" ]; then
+    result=$(grep "# MODEL  :" .config)
+else
+    result="0"
 fi
 
-make reconfig<<<${number}
-make
+result_1=$(echo ${result} | grep "${IDF_TARGET}")
+if [[ "$result_1" == "" ]]; then
+    echo Rebuild ali-smartliving-device-sdk-c lib!
+
+    make distclean
+
+    if [ $2 == esp32 ]; then
+        number=2
+    elif [ $2 == esp32c3 ]; then
+        number=3
+    elif [ $2 == esp32c6 ]; then
+        number=4
+    elif [ $2 == esp32s2 ]; then
+        number=5
+    elif [ $2 == esp32s3 ]; then
+        number=6
+    elif [ $2 == esp8266 ]; then
+        number=7
+    fi
+
+    make reconfig<<<${number}
+    make
+else
+    echo Do nothing!
+fi
+
 echo "******************** Build ali-smartliving-device-sdk-c lib END **********************"
 
-result_0=$(grep "CONFIG_IDF_TARGET=" $3/sdkconfig)
-result_1=$(echo ${result_0} | grep "${IDF_TARGET}")
+result=$(grep "CONFIG_IDF_TARGET=" $3/sdkconfig)
+result_1=$(echo ${result} | grep "${IDF_TARGET}")
 if [[ "$result_1" == "" ]]; then
     rm $3/sdkconfig
     echo RM sdkconfig!
